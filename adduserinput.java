f9e01c9d074d1576509268a2c0506f67b95731b7
Pratyush Birole <birole.pratyush@gmail.com>
2024-02-07 19:45:58 +0530

    Add files via upload

---
+++ adduserinput.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+import javax.sql.rowset.spi.SyncResolver;
+
+public class adduserinput{
+  public static void main(String[] args) {
+    System.out.println(sum);
+    
+  }
+
+
+
+
+
+    
+    }private static void sum {
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter 1st number");
+        int x=sc.nextInt();
+        System.out.println("Enter 2nd number");
+        int y=sc.nextInt();
+        System.out.println( x+y);
+}
