ef0fa3d1b75c695f6d46cf9540cecfe58002c4aa
Pratyush Birole <birole.pratyush@gmail.com>
2024-04-01 04:49:38 +0530

    Add files via upload

---
+++ Mid Sem Q - Module 1/FibbonacciNumber.java
@@ -0,0 +1,11 @@
+public class FibbonacciNumber {
+    public static void main(String[] args) {
+        
+    
+}public static int fibonacci(int n) {
+    if (n <= 1) {
+        return n;
+    }
+    return fibonacci(n - 1) + fibonacci(n - 2);
+}
+}
