fdd5e06070bd060fa2eaf9b786083f5266e73f12
Pratyush Birole <birole.pratyush@gmail.com>
2024-04-01 05:50:56 +0530

    Add files via upload

---
+++ Mid Sem Q - Module 2/CalculatorClassandMethods.java
@@ -0,0 +1,41 @@
+// Write a java program for develop the functionality of calculator using classes and methods
+// with switch statements
+
+import java.util.Scanner;
+
+public class CalculatorClassandMethods {
+   public static void main(String[] args) {
+
+    Scanner sc= new Scanner(System.in);
+    double num1 =sc.nextInt();
+    double num2= sc.nextInt();
+
+    int choice = sc.nextInt();
+
+
+    switch (choice){
+        case 1: result = Calculator.add(num1, num2);
+    }
+
+
+
+
+
+    
+   }class Calculator{
+    
+
+     public static double add(double a, double b){
+        return a+b;
+     }
+
+     public static double sub(double a, double b){
+        return a-b;
+     }
+
+
+
+
+
+   }
+}
